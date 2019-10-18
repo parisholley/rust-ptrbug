43e3861e658dc9dd41fe6297c7b227fa384a8938
Paris Holley <mail@parisholley.com>
2019-10-18 14:00:42 -0400

    more examples of assertion problems, which appear to be related to string allocation

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
+Works:
+
 ```
-cargo test panic
-cargo test no_panic
+cargo test no_panic -- --nocapture
+cargo test no_panic_str -- --nocapture
+```
+
+Broken:
+
+```
+cargo test panic_static_to_string -- --nocapture
+cargo test panic_const_to_string -- --nocapture
+cargo test panic_tostring -- --nocapture
+cargo test panic_cstring -- --nocapture
+cargo test panic_println -- --nocapture
+cargo test panic_println -- --nocapture
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate test;
 
 use crate::bind::{startEngine, engineStruct, getWorker, releaseWorker};
 use std::mem::MaybeUninit;
+use std::ffi::CString;
 
 mod bind;
 
@@ -32,6 +33,35 @@ fn do_get_release_worker(engine: &mut engineStruct) -> i32 {
     released
 }
 
+static panic_static:&str = "this static str should not cause panic";
+
+const panic_string:&str = "this const str should not cause panic";
+
+fn should_not_cause_panic_println(){
+    println!("this println does not cause a panic");
+}
+
+fn should_not_cause_panic_cstring(){
+    let cstr = CString::new("foobar").expect("CString::new failed");
+    let cstr_ptr = cstr.into_raw();
+}
+
+fn should_not_cause_panic_str(){
+    "this inline str should not cause panic";
+}
+
+fn should_not_cause_panic_static_to_string(){
+    panic_static.to_string();
+}
+
+fn should_not_cause_panic_const_to_string(){
+    panic_string.to_string();
+}
+
+fn should_not_cause_panic_to_string(){
+    "this inline str.to_string() should not cause panic".to_string();
+}
+
 impl Engine{
     pub fn new() -> Engine{
         let mut engine:MaybeUninit<engineStruct> = unsafe { MaybeUninit::uninit()};
@@ -43,10 +73,30 @@ impl Engine{
 
             let mut assumed = engine.assume_init();
 
-            println!("this println does not cause a panic");
+            should_not_cause_panic_println();
+            should_not_cause_panic_cstring();
+            should_not_cause_panic_to_string();
+            should_not_cause_panic_str();
+            should_not_cause_panic_const_to_string();
+            should_not_cause_panic_static_to_string();
 
             do_get_release_worker(&mut assumed);
+
+            should_not_cause_panic_println();
+            should_not_cause_panic_cstring();
+            should_not_cause_panic_to_string();
+            should_not_cause_panic_str();
+            should_not_cause_panic_const_to_string();
+            should_not_cause_panic_static_to_string();
+
             do_get_release_worker(&mut assumed);
+
+            should_not_cause_panic_println();
+            should_not_cause_panic_cstring();
+            should_not_cause_panic_to_string();
+            should_not_cause_panic_str();
+            should_not_cause_panic_const_to_string();
+            should_not_cause_panic_static_to_string();
 
             Engine {
                 engine: assumed
@@ -63,23 +113,79 @@ impl Engine{
 mod tests {
     use super::*;
     use test::Bencher;
-
-    #[test]
-    fn panic() {
-        let mut engine = Engine::new();
-
-        engine.get_release_worker();
-
-        println!("this println causes panic");
-
-        engine.get_release_worker();
-    }
+    use std::ffi::CString;
 
     #[test]
     fn no_panic() {
         let mut engine = Engine::new();
 
         engine.get_release_worker();
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn no_panic_str() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_str();
+
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn panic_static_to_string() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_static_to_string();
+
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn panic_const_to_string() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_const_to_string();
+
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn panic_tostring() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_to_string();
+
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn panic_cstring() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_cstring();
+
+        engine.get_release_worker();
+    }
+
+    #[test]
+    fn panic_println() {
+        let mut engine = Engine::new();
+
+        engine.get_release_worker();
+
+        should_not_cause_panic_println();
+
         engine.get_release_worker();
     }
 }
